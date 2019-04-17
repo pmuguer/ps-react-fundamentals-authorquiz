266c26515cc1209249e74f5c5b4c2346bc526c24
Pablo Muguerza <pmuguer@gmail.com>
2019-04-17 05:29:19 -0300

    4. JSX - Author Quiz

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
+import {shuffle, sample} from 'underscore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const authors = [
+    {
+        name: 'Mark Twain',
+        imageUrl: 'images/authors/mark_twain.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['The adventures of Huckleberry Finn',
+                'Life on the Missisipi',
+                'Roughing it']
+    },
+    {
+        name: 'William Shakespeare',
+        imageUrl: 'images/authors/mark_twain.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['Macbeth', 'A midnight summer\'s dream', 'Richard III']
+    }
+];
+
+function getTurnData(authors) {
+    const allBooks = authors.reduce(function(p, c, i){
+        return p.concat(c.books);
+    }, []);
+    const fourRandomBooks = shuffle(allBooks).slice(0,4);
+    const answer = sample(fourRandomBooks);
+
+    return {
+        books: fourRandomBooks,
+        author: authors.find((author) => author.books.some((title) => title === answer))
+    }
+}
+
+const state = {
+    turnData: getTurnData(authors)
+};
+
+
+ReactDOM.render(<AuthorQuiz {...state}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
